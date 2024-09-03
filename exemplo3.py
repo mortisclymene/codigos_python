14ae2928fcb77936ed037b1a35d55ec0e4e43388
Joao M <joao.durans2209@gmail.com>
2024-09-03 09:46:49 -0300

    Calculando média com python

---
+++ exemplo3.py
@@ -0,0 +1,7 @@
+nota1 = float(input ("Informe sua primeira nota: "))
+nota2 = float(input ("Informe sua segunda nota: "))
+nota3 = float(input ("Informe sua terceira nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"Sua média é {media}")
