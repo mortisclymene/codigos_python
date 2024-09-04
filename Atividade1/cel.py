2f09b9a9cae141b5c7d7203a07b8982347c3a1ac
Joao M <joao.durans2209@gmail.com>
2024-09-04 08:10:39 -0300

    Adicionando exemplo condicional

--- Atividade1/cel.py
+++ Atividade1/cel.py
@@ -1,5 +1,5 @@
 fah = float(input("Informe a temperatura em Fahrenheit: "))
 
 cel = (fah - 32) * 5/9
-
+ 
 print(f"A temperatura está a: ", cel,"C")
---
+++ exemplo-condicional.py
@@ -0,0 +1,6 @@
+valor = int(input("Informe um valor númerico:"))
+
+if valor>= 50:
+    print (f"O número {valor} é maio ou igual a 50\n")
+else:
+    print(f"O número {valor} é menor que 50\n")
